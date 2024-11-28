ecc2bad1f8331b3bb0f19ad4a9b887056a125a0d
FELIX TERRIEN <proemail78@gmail.com>
2024-11-28 11:10:39 -0500

    test

--- code/machinelearning.py
+++ code/machinelearning.py
@@ -1,10 +1,33 @@
 import pandas as pd
-from nltk.tokenize.toktok import ToktokTokenizer
-import string
+import numpy as np
+import matplotlib.pyplot as plts
+from nltk.corpus import stopwords
+import re
+
+
 
 
 
 
 dataset = pd.read_csv('dataset/IMDB_Dataset.csv')
 
+text = dataset.iloc[0]["review"]
 
+
+def rm_words(text):
+    stop_words = stopwords.words('english')
+    words = text.split()
+    new_text = ''
+    for word in words:
+        if word not in stop_words:
+            new_text += word + ' '
+    return new_text
+
+def rm_char(text):
+    text = text.lower()
+    text = re.sub(r'<.*?>', '', text)
+    text = re.sub(r'\W', ' ', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
+
+print(rm_char(rm_words(text)))
